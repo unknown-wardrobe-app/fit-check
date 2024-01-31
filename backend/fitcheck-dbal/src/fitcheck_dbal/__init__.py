b6e6eba76cc121e2746ebed361c1a82d109ef3fb
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2024-01-30 16:58:01 -0800

    Implement simple user db operation Implement a gRPC servicer to create new users and look up users with an ID Implement a gRPC client to repreatedly create a new user and look up that user with the generated ID

--- backend/fitcheck-dbal/src/fitcheck_dbal/__init__.py
+++ backend/fitcheck-dbal/src/fitcheck_dbal/__init__.py
@@ -4,3 +4,47 @@
     :module_author: Nathan Mendoza
 """
 
+
+import pymongo
+from bson import ObjectId
+
+from .messages.responses_pb2 import CreatedUser, GetUser, QueryError, PutError
+from .messages.userdoc_pb2 import UserDocument
+from .services.services_pb2_grpc import UserDocumentServiceServicer
+
+
+class UserDocumentController(UserDocumentServiceServicer):
+
+    def __init__(self):
+        self._db = pymongo.MongoClient("mongodb:127.0.0.1:9000/users")
+
+    def GetUserDocument(self, request, context) -> GetUser:
+        try:
+            result = self._db.users.find_one({"_id": ObjectId(request.userid)})
+            return GetUser(user=UserDocument(
+                userid=result['_id'],
+                username=result['username'],
+                password=result['password']
+            ))
+        except pymongo.errors.PyMongoError as e:
+            return GetUser(err=QueryError(
+                error_type=QueryError.ErrorType.UNSPECIFIED,
+                detail=str(e)
+            ))
+
+    def CreateUserDocument(self, request, context) -> CreatedUser:
+        try:
+            result = self._db.users.insert_one({
+                "username": request.username,
+                "password": request.password
+            })
+            return CreatedUser(new_user=UserDocument(
+                userid=str(result.inserted_id),
+                username=request.username,
+                password=request.password
+            ))
+        except pymongo.errors.PyMongoError as e:
+            return CreatedUser(err=PutError(
+                error_type=PutError.ErrorType.UNSPECIFIED,
+                detail=str(e)
+            ))
--- backend/fitcheck-dbal/src/fitcheck_dbal/cli.py
+++ backend/fitcheck-dbal/src/fitcheck_dbal/cli.py
@@ -5,9 +5,42 @@
 """
 
 import click
+import grpc
+from .grpc.services_pb2_grpc import (
+    add_UserDocumentServiceServicer_to_server,
+    UserDocumentServiceStub
+)
+from .grpc.requests_pb2 import NewUser, QueryForUser
+
+import futures
+
+from . import UserDocumentController
 
 
 @click.command()
 def fitcheck_dbal():
     """Entry point to fitcheck-dbal"""
     click.echo('Hello World!')
+
+
+@click.command()
+def serve():
+    port = "50051"
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    add_UserDocumentServiceServicer_to_server(UserDocumentController(), server)
+    server.add_insecure_port("[::]:" + port)
+    server.start()
+    print("Server started, listening on " + port)
+    server.wait_for_termination()
+
+
+@click.command()
+def request():
+    with grpc.insecure_channel("localhost:50051") as channel:
+        stub = UserDocumentServiceStub(channel)
+        response = stub.CreateUserDocument(
+            NewUser(username="johndoe", password="supersecretstring"))
+        print("Client received: " + response.result)
+        response = stub.GetUserDocument(
+            QueryForUser(userid=response.result.userid))
+        print("Client received: " + response.result)
