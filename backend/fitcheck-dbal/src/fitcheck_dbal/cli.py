8a8138204e85959b0faa6b276fcc89451887790c
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2024-02-15 16:38:01 -0800

    Implement small CLI to launch servicers and hit with sample requests

--- backend/fitcheck-dbal/src/fitcheck_dbal/cli.py
+++ backend/fitcheck-dbal/src/fitcheck_dbal/cli.py
@@ -5,6 +5,12 @@
 """
 
 import click
+import grpc
+from concurrent import futures
+import fitcheckprotos.dblayer.users.account_pb2_grpc as fitcheckaccountservice
+from fitcheckprotos.dblayer.users.account_pb2 import CreateAccount
+
+from .servicers import FitcheckAccountManagementServicer
 
 
 @click.group()
@@ -14,35 +20,27 @@ def fitcheck_dbal():
 
 
 @fitcheck_dbal.command()
-def hello():
-    click.echo('Hello!')
-
-
-@fitcheck_dbal.command()
-def goodbye():
-    click.echo('Goodbye!')
-
-
-'''
-def serve():
-    port = "50051"
+@click.option('-p', '--port', default="50051")
+def serve(port):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    add_UserDocumentServiceServicer_to_server(UserDocumentController(), server)
+    fitcheckaccountservice.add_AccountManagementServicer_to_server(
+        FitcheckAccountManagementServicer(),
+        server
+    )
     server.add_insecure_port("[::]:" + port)
     server.start()
-    print("Server started, listening on " + port)
+    print(f"Server listening on port: {port}")
     server.wait_for_termination()
 
 
-def request():
+@fitcheck_dbal.command()
+@click.option('-u', '--username', required=True)
+@click.option('-p', '--password', required=True)
+def makeuser(username, password):
     with grpc.insecure_channel("localhost:50051") as channel:
-        stub = UserDocumentServiceStub(channel)
-        response = stub.CreateUserDocument(
-            NewUser(username="johndoe", password="supersecretstring"))
-        print("Client received: ", response.new_user.userid)
-        time.sleep(2)
-        response = stub.GetUserDocument(
-            QueryForUser(userid=str(response.new_user.userid))
-        )
-        print("Client received: ", response)
-'''
+        stub = fitcheckaccountservice.AccountManagementStub(channel)
+        response = stub.NewAccount(CreateAccount(
+            username=username,
+            password=password
+        ))
+        print(f"New user ID: {response.user.id}")
