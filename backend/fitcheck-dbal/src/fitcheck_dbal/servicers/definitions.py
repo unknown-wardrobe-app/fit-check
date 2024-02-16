b3299f1a2e14f9df1ac83e44996e1080da9fe55a
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2024-02-15 16:37:36 -0800

    Document additional servicers for user document management

---
+++ backend/fitcheck-dbal/src/fitcheck_dbal/servicers/definitions.py
@@ -0,0 +1,5 @@
+"""
+    :module_name: dbops
+    :module_summary: gRPC servicer implementation for user defined value management
+    :module_author: Nathan Mendoza
+"""
---
+++ backend/fitcheck-dbal/src/fitcheck_dbal/servicers/spaces.py
@@ -0,0 +1,5 @@
+"""
+    :module_name: dbops
+    :module_summary: gRPC servicer implementation for closet space management
+    :module_author: Nathan Mendoza
+"""
