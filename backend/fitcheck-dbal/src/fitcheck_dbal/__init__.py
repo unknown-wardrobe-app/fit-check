375963142c867d459b774c9daf0d983b14dc0e48
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2024-02-15 16:34:19 -0800

    Purpose changed to package setup
    Currently only sets up logging
    May be extended to handle server configuration later on

--- backend/fitcheck-dbal/src/fitcheck_dbal/__init__.py
+++ backend/fitcheck-dbal/src/fitcheck_dbal/__init__.py
@@ -4,55 +4,15 @@
     :module_author: Nathan Mendoza
 """
 
+import logging
 
-'''
-import pymongo
-from bson import ObjectId
-
-from dbal_pb2 import CreatedUser, GetUser, QueryError, PutError, UserDocument
-from dbal_pb2_grpc import UserDocumentServiceServicer
-
-
-class UserDocumentController(UserDocumentServiceServicer):
-
-    def __init__(self):
-        self._db = pymongo.MongoClient("mongodb://127.0.0.1:9000/fitcheck")
-
-    def GetUserDocument(self, request, context) -> GetUser:
-        try:
-            result = self._db.fitcheck.users.find_one(
-                {"_id": ObjectId(request.userid)}
-            )
-            if result:
-                return GetUser(user=UserDocument(
-                    userid=str(result['_id']),
-                    username=result['username'],
-                    password=result['password']
-                ))
-            return GetUser(
-                err=QueryError.ErrorType.NO_SUCH_USER,
-                detail=f'No such user with ID: {request.userid}'
-            )
-        except pymongo.errors.PyMongoError as e:
-            return GetUser(err=QueryError(
-                error_type=QueryError.ErrorType.UNSPECIFIED,
-                detail=str(e)
-            ))
-
-    def CreateUserDocument(self, request, context) -> CreatedUser:
-        try:
-            result = self._db.fitcheck.users.insert_one({
-                "username": request.username,
-                "password": request.password
-            })
-            return CreatedUser(new_user=UserDocument(
-                userid=str(result.inserted_id),
-                username=request.username,
-                password=request.password
-            ))
-        except pymongo.errors.PyMongoError as e:
-            return CreatedUser(err=PutError(
-                error_type=PutError.ErrorType.UNSPECIFIED,
-                detail=str(e)
-            ))
-'''
+LOGGER = logging.getLogger(__name__)
+LOG_HANDLER = logging.StreamHandler()
+LOG_FORMAT = logging.Formatter(
+    fmt='[%(asctime)s] [%(process)d] [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S %z'
+)
+LOGGER.setLevel(logging.DEBUG)
+LOG_HANDLER.setLevel(logging.DEBUG)
+LOG_HANDLER.setFormatter(LOG_FORMAT)
+LOGGER.addHandler(LOG_HANDLER)
